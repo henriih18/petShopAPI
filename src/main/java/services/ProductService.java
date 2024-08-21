56c96e24df8fe43cf72fd8f1f8316b714dd84685
henriih18 <hernandezhenry58@gmail.com>
2024-08-21 16:35:03 -0500

    creacion

---
+++ src/main/java/services/ProductService.java
@@ -0,0 +1,27 @@
+package services;
+
+import org.springframework.stereotype.Service;
+import repository.ProductRepository;
+
+@Service
+public class ProductService {
+    private final ProductRepository productRepository;
+
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product saveProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+}
+
